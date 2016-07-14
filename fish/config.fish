2b36e94b4c0eb966ff9dcd1f42d7d0281f5af01c
shofel <v.istomin@examus.net>
2016-07-14 18:41:31 +0500

    fish: autocompletion for gulp tasks

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+# @see https://github.com/akoenig/gulp.plugin.zsh/blob/master/gulp.plugin.zsh
+function gulptasks
+        set -l gulpfiles (find . -maxdepth 1 -name 'gulpfile.*')
+
+        if test (count $gulpfiles) -gt 0
+          grep -Eho "gulp\.task[^,]*" $gulpfiles ^ /dev/null \
+          | sed s/\"/\'/g \
+          | cut -d "'" -f 2 \
+          | sort
+        end
+end
+
+complete --command gulp --arguments "(gulptasks)" --no-files
