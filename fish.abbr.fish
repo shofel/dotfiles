a99a39246a653880c36dfadbbcf46046a719e1c7
shofel <v.istomin@examus.net>
2017-07-25 15:07:53 +0500

    fish abbr

---
+++ fish.abbr.fish
@@ -0,0 +1,43 @@
+# git update
+abbr gfa 'git fetch --all --prune --tags'
+abbr gl 'git pull'
+abbr gp 'git push'
+abbr gpf 'git push --force-with-lease'
+
+# git checkout
+abbr gco 'git checkout'
+abbr gcm 'git checkout master'
+abbr gcb 'git checkout -b'
+abbr grh 'git reset HEAD'
+abbr gb 'git branch'
+
+# git rebase
+abbr grb 'git rebase'
+abbr grba 'git rebase --abort'
+abbr grbc 'git rebase --continue'
+abbr grbi 'git rebase -i'
+abbr grbs 'git rebase --skip'
+abbr gmt 'git mergetool'
+
+# commit
+abbr gd 'git diff'
+abbr gst 'git status --short'
+abbr gdca 'git diff --cached'
+abbr ga 'git add'
+abbr gcom 'git commit'
+abbr gamend 'git commit --amend --no-edit'
+
+# housekeeping
+abbr gclean 'git clean -fd'
+abbr ghash 'git rev-parse --short HEAD'
+abbr ginit 'git init ;and git commit -m "root" --allow-empty'
+abbr gcd 'cd (git rev-parse --show-toplevel)'
+
+# keyboard
+abbr dvorak 'setxkbmap -model pc104 -layout us,ru -variant dvorak, -option grp:alt_shift_toggle'
+abbr nocaps 'setxkbmap -option ctrl:nocaps'
+
+# fun
+abbr dc docker-compose
+abbr weather 'curl wttr.in/guangzhou'
+abbr suspend "clear; echo 'Cover the keyboard, dude!'; sleep 10; and echo 'suspended'; and xscreensaver-command -lock; and sudo pm-suspend-hybrid"
