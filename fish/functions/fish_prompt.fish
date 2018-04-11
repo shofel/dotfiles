53d055390041b7200be2dad9b7fa47b5ae44d5e0
Vyacheslav Istomin <visla.vvi@gmail.com>
2018-04-12 02:53:20 +0500

    fish: tidy up prompt function; remove braces around git status.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,10 @@
-# vim: set ts=2 sts=2 noet :
+# vim: ts=2 sts=2 noet :
+#
+# SEE for details: https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_git_prompt.fish
 function fish_prompt --description 'Write out the prompt'
+
+	# TODO check if the vars are already declared
+	# @see https://github.com/fish-shell/fish-shell/blob/627ce4ea34f180745003f50c540a17cbfc5a22ba/share/tools/web_config/sample_prompts/classic_vcs.fish#L37
 
 	set -g __fish_git_prompt_show_informative_status 1
 	set -g __fish_git_prompt_hide_untrackedfiles 1
@@ -7,21 +12,13 @@ function fish_prompt --description 'Write out the prompt'
 
 	set -g __fish_git_prompt_showupstream "informative"
 
-	set -g __fish_git_prompt_char_upstream_ahead "↑"
-	set -g __fish_git_prompt_char_upstream_behind "↓"
-	set -g __fish_git_prompt_char_upstream_prefix ""
-
-	set -g __fish_git_prompt_char_stagedstate "●"
-	set -g __fish_git_prompt_char_dirtystate "✚"
-	set -g __fish_git_prompt_char_untrackedfiles "…"
-	set -g __fish_git_prompt_char_conflictedstate "✖"
-	set -g __fish_git_prompt_char_cleanstate "✔"
-
 	set -g __fish_git_prompt_color_dirtystate blue
 	set -g __fish_git_prompt_color_stagedstate yellow
 	set -g __fish_git_prompt_color_invalidstate red
 	set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
 	set -g __fish_git_prompt_color_cleanstate green --bold
+
+	set -g __fish_git_prompt_char_stateseparator ' '
 
 	set -l last_status $status
 
@@ -45,20 +42,22 @@ function fish_prompt --description 'Write out the prompt'
 	# Indicate failed commands.
 	set -l color_status $fish_color_cwd
 	if test $last_status -ne 0
-		set color_status red --bold
+		set color_status $fish_color_error
 	end
+
+	#
+	# Print
+	#
 
 	# PWD
 	set_color $color_cwd
 	echo -n (prompt_pwd)
 	set_color normal
 
-	printf '%s ' (__fish_vcs_prompt)
+	# Git
+	__fish_git_prompt ' %s '
 
-	if not test $last_status -eq 0
-	set_color $fish_color_error
-	end
-
+	# Suffix
 	set_color $color_status
 	echo -n "$suffix "
 
