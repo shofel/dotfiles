73a467ca00cb0273617dc58f092404875d74083b
Vyacheslav Istomin <visla.vvi@gmail.com>
2018-04-12 01:30:34 +0500

    fish: clean up fish_prompt.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,64 +1,31 @@
+# vim: set ts=2 sts=2 noet :
 function fish_prompt --description 'Write out the prompt'
-	if not set -q __fish_git_prompt_show_informative_status
-		set -g __fish_git_prompt_show_informative_status 1
-	end
-	if not set -q __fish_git_prompt_hide_untrackedfiles
-		set -g __fish_git_prompt_hide_untrackedfiles 1
-	end
 
-	if not set -q __fish_git_prompt_color_branch
-		set -g __fish_git_prompt_color_branch magenta --bold
-	end
-	if not set -q __fish_git_prompt_showupstream
-		set -g __fish_git_prompt_showupstream "informative"
-	end
-	if not set -q __fish_git_prompt_char_upstream_ahead
-		set -g __fish_git_prompt_char_upstream_ahead "↑"
-	end
-	if not set -q __fish_git_prompt_char_upstream_behind
-		set -g __fish_git_prompt_char_upstream_behind "↓"
-	end
-	if not set -q __fish_git_prompt_char_upstream_prefix
-		set -g __fish_git_prompt_char_upstream_prefix ""
-	end
+	set -g __fish_git_prompt_show_informative_status 1
+	set -g __fish_git_prompt_hide_untrackedfiles 1
+	set -g __fish_git_prompt_color_branch magenta --bold
 
-	if not set -q __fish_git_prompt_char_stagedstate
-		set -g __fish_git_prompt_char_stagedstate "●"
-	end
-	if not set -q __fish_git_prompt_char_dirtystate
-		set -g __fish_git_prompt_char_dirtystate "✚"
-	end
-	if not set -q __fish_git_prompt_char_untrackedfiles
-		set -g __fish_git_prompt_char_untrackedfiles "…"
-	end
-	if not set -q __fish_git_prompt_char_conflictedstate
-		set -g __fish_git_prompt_char_conflictedstate "✖"
-	end
-	if not set -q __fish_git_prompt_char_cleanstate
-		set -g __fish_git_prompt_char_cleanstate "✔"
-	end
+	set -g __fish_git_prompt_showupstream "informative"
 
-	if not set -q __fish_git_prompt_color_dirtystate
-		set -g __fish_git_prompt_color_dirtystate blue
-	end
-	if not set -q __fish_git_prompt_color_stagedstate
-		set -g __fish_git_prompt_color_stagedstate yellow
-	end
-	if not set -q __fish_git_prompt_color_invalidstate
-		set -g __fish_git_prompt_color_invalidstate red
-	end
-	if not set -q __fish_git_prompt_color_untrackedfiles
-		set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-	end
-	if not set -q __fish_git_prompt_color_cleanstate
-		set -g __fish_git_prompt_color_cleanstate green --bold
-	end
+	set -g __fish_git_prompt_char_upstream_ahead "↑"
+	set -g __fish_git_prompt_char_upstream_behind "↓"
+	set -g __fish_git_prompt_char_upstream_prefix ""
+
+	set -g __fish_git_prompt_char_stagedstate "●"
+	set -g __fish_git_prompt_char_dirtystate "✚"
+	set -g __fish_git_prompt_char_untrackedfiles "…"
+	set -g __fish_git_prompt_char_conflictedstate "✖"
+	set -g __fish_git_prompt_char_cleanstate "✔"
+
+	set -g __fish_git_prompt_color_dirtystate blue
+	set -g __fish_git_prompt_color_stagedstate yellow
+	set -g __fish_git_prompt_color_invalidstate red
+	set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+	set -g __fish_git_prompt_color_cleanstate green --bold
 
 	set -l last_status $status
 
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
-	end
+	set -g __fish_prompt_normal (set_color normal)
 
 	set -l color_cwd
 	set -l prefix
@@ -72,7 +39,7 @@ function fish_prompt --description 'Write out the prompt'
 		set suffix '#'
 	case '*'
 		set color_cwd $fish_color_cwd
-		set suffix '$'
+		set suffix '❯'
 	end
 
 	# PWD
