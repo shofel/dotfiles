65b7c6c48664896a6b934b838676c349b9f469a9
Vyacheslav Istomin <visla.vvi@gmail.com>
2018-04-12 14:16:04 +0500

    fish: clean config.fish
    - remove gulptasks completion
    - fix up appending the local dir to the path.

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,3 @@
-# @see https://github.com/akoenig/gulp.plugin.zsh/blob/master/gulp.plugin.zsh
-set PATH ~/.local/bin/ $PATH
-
-function gulptasks
-        set -l gulpfiles (find . -maxdepth 1 -name 'gulpfile.*')
-
-        if test (count $gulpfiles) -gt 0
-          grep -Eho "gulp\.task[^,]*" $gulpfiles ^ /dev/null \
-          | sed s/\"/\'/g \
-          | cut -d "'" -f 2 \
-          | sort
-        end
-end
-
-complete --command gulp --arguments "(gulptasks)" --no-files
+# To set up user defined PATH elements
+# use universal variable $fish_user_path
+# see https://fishshell.com/docs/current/tutorial.html#tut_path
