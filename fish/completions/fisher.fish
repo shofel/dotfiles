d91d3483ccb5a3ac3a91496803c607f84438d5c9
Vyacheslav Istomin <visla.vvi@gmail.com>
2018-05-07 19:28:44 +0500

    fish: fisher: track and ignore.

---
+++ fish/completions/fisher.fish
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a04d1788b52587bed0547fa7e92297b39ee52e80a7b5e1c2cf695b6c2116623f
+size 18
---
+++ fish/functions/fisher.fish
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4899f87a86a8e5de3e1aa7d761f58da6f9daf0313d1112d7fa642025b5ea6680
+size 62583
