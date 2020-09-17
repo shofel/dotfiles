209ceadc4fcdd3e8d1c0f2a73d6b0a3567f3a3e8
Vyacheslav Istomin <visla.vvi@gmail.com>
2020-09-17 15:00:50 +0500

    config.fish: fixup ssh-agent (yes, again).

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,9 @@ source ~/opt/asdf/asdf.fish
 # Assure an agent is running.
 if ! pgrep ssh-agent > /dev/null; ssh-agent -t 1h; end
 
+set -eg SSH_AGENT_PID
+set -eg SSH_AUTH_SOCK
 # Connect to the agent, mimicking output of `ssh-agent`.
 # -U means universally; -x means export
-set -Ux SSH_AGENT_PID (pgrep ssh-agent)
+set -Ux SSH_AGENT_PID (pgrep ssh-agent | sed -n '1p')
 set -Ux SSH_AUTH_SOCK /tmp/ssh-*/agent.(math $SSH_AGENT_PID - 1)
