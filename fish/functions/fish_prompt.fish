32817818689f62ca81b84e282899e40ccd336c79
Vyacheslav Istomin <visla.vvi@gmail.com>
2021-07-05 15:52:00 +0500

    fish: clean up prompt funciton.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,18 +1,4 @@
-# vim: ts=2 sts=2 noet :
-#
-# SEE
-# __fish_git_prompt sources
-# https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_git_prompt.fish
-#
-# SEE
-# nice command composition
-# https://github.com/oh-my-fish/theme-mars/blob/master/fish_prompt.fish
-#
-# TODO current time: pale and at the right side
-# SEE https://github.com/oh-my-fish/theme-trout
-#
-# TODO command execution duration when
-#
+# Defined in /tmp/fish.um6jVT/fish_prompt.fish @ line 2
 function fish_prompt --description 'Write out the prompt'
 
 	# TODO check if the vars are already declared
@@ -32,46 +18,40 @@ function fish_prompt --description 'Write out the prompt'
 
 	set -g __fish_git_prompt_char_stateseparator ' '
 
-	set -l last_status $status
-
 	set -g __fish_prompt_normal (set_color normal)
 
-	function suffix -S --description "Prompt trailer. + Indicates status."
-		# Color: error or clean.
-		set -l color
-		if test $last_status -eq 0
-			set color $fish_color_cwd
-		else
-			set color $fish_color_error
-		end
+	set -l last_status $status
 
-		# Error code. Empty if status is 0.
-		set -l error_code
+	function status_ -S --description "Indicate status code of the last command."
 		if test $last_status -ne 0
-			set error_code $last_status
+			set_color $fish_color_error
+			echo -n $last_status
 		end
-
-		# Other options: ⯈ ⮞ ⮚ ⮞
-		set -l suffix_symbol ➤
-
-		# Print
-		echo -ns (set_color $color) $error_code $suffix_symbol ' '
 	end
 
+	function suffix --description "Prompt trailer."
+		set_color $fish_color_cwd
+		echo -n '➤ '
+	end
+
+	function nix_shell_marker
+		if test -n "$IN_NIX_SHELL"
+			echo -n "<nix-shell> "
+		end
+	end
+
+	function _pwd
+		set_color $fish_color_cwd
+		echo -n (prompt_pwd) ''
+	end
 
 	#
 	# Print
 	#
 
-	# PWD
-	set_color $fish_color_cwd
-	echo -n (prompt_pwd) ''
-	set_color normal
-
-	# Git
+	_pwd
 	__fish_git_prompt ' %s '
-
-	# Suffix
+	status_
 	suffix
 
 	set_color normal
