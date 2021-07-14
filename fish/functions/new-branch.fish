9578619cac45adb92b4acbbc7d815f62d0368c12
Vyacheslav Istomin <visla.vvi@gmail.com>
2021-07-14 18:28:38 +0500

    fish: new-branch function: append today date to the branch name.

--- fish/functions/new-branch.fish
+++ fish/functions/new-branch.fish
@@ -1,7 +1,7 @@
-# Defined in /run/user/1000/fish.mXJ7dj/new-branch.fish @ line 2
 function new-branch --description 'Set up new branch: 1.create 2.push 3.MergeRequest' --argument branch
-  git switch -c $branch
-  git push -u origin $branch 2>&1 |
+  set -l date (date +%Y-%m-%-d)
+  git switch -c $branch-$date
+  git push -u origin $branch-$date 2>&1 |
     grep merge_requests/new |
     awk '{print $2}' |
     xargs xdg-open
