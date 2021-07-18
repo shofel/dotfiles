312d80029178cd82718cc13a75e75780b4629e7b
Slava <visla.vvi@gmail.com>
2021-07-18 12:21:23 +0500

    fish: easier start of ssh-agent.

--- fish/config.fish
+++ fish/config.fish
@@ -6,28 +6,6 @@
 #
 fish_add_path ~/opt/bin ~/opt/node_packages/node_modules/.bin
 
-# SSH agent.
-#
-
-# Sometimes global vars are annoyingly set by someone.
-set -ge SSH_AUTH_SOCK
-set -ge SSH_AGENT_PID
-
-# Check if ssh-agent is up and connected.
-ssh-add -L 2>&1 >/dev/null
-
-if test $status -eq 2
-  set out (ssh-agent -t 1h -c | awk '{gsub(";", ""); print $3}')
-
-  # Connect to the agent, mimicking output of `ssh-agent`.
-  # -U means universally. That is connect all fish instances.
-  set -Ux SSH_AUTH_SOCK $out[1]
-  set -Ux SSH_AGENT_PID $out[2]
-
-  echo got new ssh-agent: pid=\|$SSH_AUTH_SOCK\| sock=\|$SSH_AGENT_PID\|
-end
-
-
 # HiDPI
 #
 set -Ux GDK_SCALE 2
