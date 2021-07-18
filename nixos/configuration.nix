218f07ed76175c417dceac7b59c8982dff7d90ff
Slava <visla.vvi@gmail.com>
2021-07-18 17:44:58 +0500

    nixos: home-manage fish abbrs.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -55,8 +55,6 @@
   # services.xserver.libinput.enable = true;
 
   # Shell for all users.
-  programs.fish.enable = true;
-  users.defaultUserShell = pkgs.fish;
   environment.binsh = "${pkgs.dash}/bin/dash";
 
   programs.ssh.startAgent = true;
@@ -64,6 +62,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.shovel = {
     isNormalUser = true;
+    shell = pkgs.fish;
     extraGroups = [ "wheel" ];
   };
 
@@ -81,16 +80,48 @@
     programs.command-not-found.enable = true;
 
     home.file = {
-      ".config/fish" = {
-        source = /home/shovel/w/dotfiles/fish/config.fish;
-        target = ".config/fish/config.fish";
-      };
-
       ".config/fish/functions" = {
         source = /home/shovel/w/dotfiles/fish/functions;
         target = ".config/fish/functions";
         recursive = true;
       };
+    };
+
+    programs.fish.enable = true;
+
+    # TODO another way to keep abbrs in sync is `home.file.<name>.onChange`.
+    programs.fish.shellAbbrs = {
+      dc = "docker-compose";
+      dvorak = "setxkbmap -model pc104 -layout us,ru -variant dvorak, -option grp:alt_shift_toggle";
+      execlip = "fish -c (xclip -o)";
+      gB = "git switch (git fetch --all 1>/dev/null ;and git branch --all | string replace 'remotes/origin/' '' | string trim | sort | uniq | fzf)";
+      gb = "git switch (git branch | string trim | fzf)";
+      gBFG = "git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D";
+      ga = "git add";
+      gamend = "git commit --amend --no-edit";
+      gcb = "git switch -c";
+      gcd = "cd (git rev-parse --show-toplevel)";
+      gclean = "git clean -fd";
+      gcom = "git commit";
+      gd = "git diff";
+      gdca = "git diff --cached";
+      gfa = "git fetch --all --prune --tags";
+      ghash = "git rev-parse --short HEAD";
+      ginit = "git init ;and git commit -m 'root' --allow-empty";
+      gl = "git pull";
+      gp = "git push";
+      gpf = "git push --force-with-lease";
+      gpu = "git push -u origin (git branch | grep \* | awk \"{print \$2}\")";
+      grba = "git rebase --abort";
+      grbc = "git rebase --continue";
+      grbs = "git rebase --skip";
+      gsm = "git switch master";
+      gst = "git status --short --branch";
+      gsw = "git switch";
+      nocaps = "setxkbmap -option ctrl:nocaps";
+      suspend = "systemctl suspend";
+      v = "nvim '+Term fish'";
+      weather = "curl wttr.in/guangzhou";
     };
   };
 
