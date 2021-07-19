af1f035b3e13645111fadfdfb7ab3e98fa229aa7
Vyacheslav Istomin <visla.vvi@gmail.com>
2021-07-19 22:51:54 +0500

    home manager: backport home.nix from the `nixos` branch.

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -9,6 +9,83 @@
   home.username = "shovel";
   home.homeDirectory = "/home/shovel";
 
+  # Home Manager
+  useGlobalPkgs = true;
+  home.packages = with pkgs; [ htop kitty neovim gh bat fd ripgrep ];
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+
+    userName = "Slava";
+    userEmail = "visla.vvi@gmail.com";
+
+    extraConfig = {
+      pull = { rebase = true; };
+      core = {
+        editor = "nvim";
+        excludesfile = /home/shovel/.config/git/gitignore_global;
+      };
+    };
+  };
+
+  programs.command-not-found.enable = true;
+
+    # Fish
+
+    home.file = {
+      ".config/fish/functions" = {
+        source = /home/shovel/w/dotfiles/fish/functions;
+        recursive = true;
+      };
+    };
+
+    programs.fish.enable = true;
+
+    # TODO another way to keep abbrs in sync is `home.file.<name>.onChange`.
+    # TODO reorganize such a way that everything of git is staying together.
+    programs.fish.shellAbbrs = {
+      dc = "docker-compose";
+      execlip = "fish -c (xclip -o)";
+      # git
+      gB = "git switch (git fetch --all 1>/dev/null ;and git branch --all | string replace 'remotes/origin/' '' | string trim | sort | uniq | fzf)";
+      gb = "git switch (git branch | string trim | fzf)";
+      gBFG = "git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D";
+      ga = "git add";
+      gamend = "git commit --amend --no-edit";
+      gcb = "git switch -c";
+      gcd = "cd (git rev-parse --show-toplevel)";
+      gclean = "git clean -fd";
+      gcom = "git commit";
+      gd = "git diff";
+      gdca = "git diff --cached";
+      gfa = "git fetch --all --prune --tags";
+      ghash = "git rev-parse --short HEAD";
+      ginit = "git init ;and git commit -m 'root' --allow-empty";
+      gl = "git pull";
+      gp = "git push";
+      gpf = "git push --force-with-lease";
+      gpu = "git push -u origin (git branch | grep '*' | awk '{print \$2}')";
+      grba = "git rebase --abort";
+      grbc = "git rebase --continue";
+      grbs = "git rebase --skip";
+      gsm = "git switch master";
+      gst = "git status --short --branch";
+      gsw = "git switch";
+      #
+      nocaps = "setxkbmap -option ctrl:nocaps";
+      suspend = "systemctl suspend";
+      v = "nvim '+Term fish'";
+      weather = "curl wttr.in/guangzhou";
+    };
+
+    services.redshift = {
+      enable = true;
+      latitude = 56.83;
+      longitude = 60.60;
+      temperature = { day = 6500; night = 3000; };
+    };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
