2a96f1e6fa900f26c5a3f9242321b2413043be37
Vyacheslav Istomin <visla.vvi@gmail.com>
2021-10-19 16:03:14 +0500

    fish function new-branch: zero-padded date, gitlab-friendly branch name.

--- fish/functions/new-branch.fish
+++ fish/functions/new-branch.fish
@@ -1,7 +1,10 @@
 function new-branch --description 'Set up new branch: 1.create 2.push 3.MergeRequest' --argument branch
-  set -l date (date +%Y-%m-%-d)
-  git switch -c $branch-$date
-  git push -u origin $branch-$date 2>&1 |
+  set -l date (date +%Y-%m-%d)
+  set -l branch_name x-0-$branch-$date
+
+  git switch -c $branch_name
+
+  git push -u origin $branch_name 2>&1 |
     grep merge_requests/new |
     awk '{print $2}' |
     xargs xdg-open
