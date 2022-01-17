fd33f4ce45f72dc92ac122def4682d0494d3a44d
Vyacheslav Istomin <visla.vvi@gmail.com>
2022-01-17 19:52:47 +0500

    home.nix: fixup indentation.

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -7,6 +7,8 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs.command-not-found.enable = true;
+
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "shovel";
@@ -51,8 +53,6 @@
     };
   };
 
-  programs.command-not-found.enable = true;
-
   # neovim
   #
   home.file."plug.vim" = {
@@ -69,84 +69,84 @@
     onChange = "nvim --headless +PlugClean! +PlugInstall +qa";
   };
 
-    # Fish
+  # Fish
 
-    home.file = {
-      ".config/fish/functions" = {
-        source = ../fish/functions;
-        recursive = true;
-      };
+  home.file = {
+    ".config/fish/functions" = {
+      source = ../fish/functions;
+      recursive = true;
     };
+  };
 
-    programs.fish =
-      let
-        configExtra = ''
-          set -U VISUAL ${pkgs.neovim}/bin/nvim
+  programs.fish =
+    let
+      configExtra = ''
+        set -U VISUAL ${pkgs.neovim}/bin/nvim
 
-          # ssh ask pass program
-          set -Ux SSH_ASKPASS ${pkgs.ssh-askpass-fullscreen}/bin/ssh-askpass-fullscreen
+        # ssh ask pass program
+        set -Ux SSH_ASKPASS ${pkgs.ssh-askpass-fullscreen}/bin/ssh-askpass-fullscreen
 
-          # kitty and some other programs
-          fish_add_path ~/opt/bin
+        # kitty and some other programs
+        fish_add_path ~/opt/bin
 
-          # HiDPI
-          set -Ux GDK_SCALE 2
+        # HiDPI
+        set -Ux GDK_SCALE 2
 
-          # Nix
-          #
-          fish_add_path /nix/var/nix/profiles/per-user/shovel/profile/bin
-          set -x --unpath NIX_PATH (string join ':' \
-            home-manager=/home/shovel/.nix-defexpr/channels/home-manager \
-            nixpkgs=/home/shovel/.nix-defexpr/channels/nixpkgs)
-        '';
-        shellAbbrs = {
-          dc = "docker-compose";
-          execlip = "fish -c (xclip -o)";
-          # git
-          gB = "git switch (git fetch --all 1>/dev/null ;and git branch --all | string replace 'remotes/origin/' '' | string trim | sort | uniq | fzf)";
-          gb = "git switch (git branch | string trim | fzf)";
-          gBFG = "git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D";
-          ga = "git add";
-          gamend = "git commit --amend --no-edit";
-          gcb = "git switch -c";
-          gcd = "cd (git rev-parse --show-toplevel)";
-          gclean = "git clean -fd";
-          gcom = "git commit";
-          gd = "git diff";
-          gdca = "git diff --cached";
-          gfa = "git fetch --all --prune --tags";
-          ghash = "git rev-parse --short HEAD";
-          ginit = "git init ;and git commit -m 'root' --allow-empty";
-          gl = "git pull";
-          gp = "git push";
-          gpf = "git push --force-with-lease";
-          gpu = "git push -u origin (git branch | grep '*' | awk '{print \$2}')";
-          grba = "git rebase --abort";
-          grbc = "git rebase --continue";
-          grbs = "git rebase --skip";
-          gsm = "git switch master";
-          gst = "git status --short --branch";
-          gsw = "git switch";
-          #
-          nocaps = "setxkbmap -option ctrl:nocaps";
-          suspend = "systemctl suspend";
-          v = "nvim '+Term fish'";
-          weather = "curl wttr.in/guangzhou";
-        };
-      in {
-        enable = true;
-        interactiveShellInit =
-          configExtra +
-          builtins.readFile ../fish/ssh-agent.fish;
-        shellAbbrs = shellAbbrs;
+        # Nix
+        #
+        fish_add_path /nix/var/nix/profiles/per-user/shovel/profile/bin
+        set -x --unpath NIX_PATH (string join ':' \
+          home-manager=/home/shovel/.nix-defexpr/channels/home-manager \
+          nixpkgs=/home/shovel/.nix-defexpr/channels/nixpkgs)
+      '';
+      shellAbbrs = {
+        dc = "docker-compose";
+        execlip = "fish -c (xclip -o)";
+        # git
+        gB = "git switch (git fetch --all 1>/dev/null ;and git branch --all | string replace 'remotes/origin/' '' | string trim | sort | uniq | fzf)";
+        gb = "git switch (git branch | string trim | fzf)";
+        gBFG = "git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D";
+        ga = "git add";
+        gamend = "git commit --amend --no-edit";
+        gcb = "git switch -c";
+        gcd = "cd (git rev-parse --show-toplevel)";
+        gclean = "git clean -fd";
+        gcom = "git commit";
+        gd = "git diff";
+        gdca = "git diff --cached";
+        gfa = "git fetch --all --prune --tags";
+        ghash = "git rev-parse --short HEAD";
+        ginit = "git init ;and git commit -m 'root' --allow-empty";
+        gl = "git pull";
+        gp = "git push";
+        gpf = "git push --force-with-lease";
+        gpu = "git push -u origin (git branch | grep '*' | awk '{print \$2}')";
+        grba = "git rebase --abort";
+        grbc = "git rebase --continue";
+        grbs = "git rebase --skip";
+        gsm = "git switch master";
+        gst = "git status --short --branch";
+        gsw = "git switch";
+        #
+        nocaps = "setxkbmap -option ctrl:nocaps";
+        suspend = "systemctl suspend";
+        v = "nvim '+Term fish'";
+        weather = "curl wttr.in/guangzhou";
       };
-
-    services.redshift = {
+    in {
       enable = true;
-      latitude = 56.83;
-      longitude = 60.60;
-      temperature = { day = 6500; night = 3000; };
+      interactiveShellInit =
+        configExtra +
+        builtins.readFile ../fish/ssh-agent.fish;
+      shellAbbrs = shellAbbrs;
     };
+
+  services.redshift = {
+    enable = true;
+    latitude = 56.83;
+    longitude = 60.60;
+    temperature = { day = 6500; night = 3000; };
+  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
