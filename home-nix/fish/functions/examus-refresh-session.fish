76ce4a0123d91f09c11e3801be0c9386c0a96851
Vyacheslav Istomin <visla.vvi@gmail.com>
2022-02-10 14:12:08 +0500

    fish functions: fixup usage of simple-integration-client.

--- home-nix/fish/functions/examus-refresh-session.fish
+++ home-nix/fish/functions/examus-refresh-session.fish
@@ -1,4 +1,4 @@
 #
 function examus-refresh-session
-  ~/workspaces/Examus/simple-integration-client/simple_integration_client.py use
+  ~/workspaces/Examus/simple-integration-client/simple_integration_client.py make
 end
