2e487fd7b3ed56e6f0c32f6fa1e613a91edb8f92
Slava <visla.vvi@gmail.com>
2022-07-16 15:03:59 +0300

    home manager: recover from broken config; nix flake update

--- home-nix/flake.nix
+++ home-nix/flake.nix
@@ -30,276 +30,282 @@
       neovim-package = inputs.neovim.packages.${system}.neovim;
     in {
       slava = inputs.home-manager.lib.homeManagerConfiguration {
-        system = system;
-        homeDirectory = "/home/slava"; # TODO linux /home/slava
-        username = "slava";
-        stateVersion = "21.05";
+        inherit pkgs;
 
-        configuration = {
-          imports = [ ];
-
-          programs.home-manager.enable = true;
-          programs.command-not-found.enable = true;
-
-          # home.packages {{{
-          home.packages = let
-            main = with pkgs; [
-              jq
-              gh
-              fd
-              ripgrep
-
-              htop
-
-              nixfmt
-              rnix-lsp
-              sumneko-lua-language-server
-              terraform-ls
-
-              neovim-package
-
-              fira-code # for kitty
-            ];
-            node = with pkgs.nodePackages; [
-              vim-language-server
-              vscode-langservers-extracted
-              yaml-language-server
-            ];
-          in main ++ node;
-          # }}} home.packages
-
-          # {{{ fzf
-          programs.fzf = {
-            enable = true;
-            enableFishIntegration = true;
-          };
-          # }}} fzf
-
-          # {{{ bat
-          programs.bat = {
-            enable = true;
-            config = {
-              map-syntax = [ "*.json5:JavaScript (Babel)" ];
-              style = "numbers";
-              theme = bat-theme;
+        modules = [
+          {
+            home = {
+              username = "slava";
+              homeDirectory = "/home/slava"; # TODO linux /home/slava
+              stateVersion = "21.05";
             };
-          };
-          # }}} bat
+          }
+          {
+            programs.home-manager.enable = true;
+            programs.command-not-found.enable = true;
 
-          # git {{{
-          programs.git = {
-            enable = true;
-            lfs.enable = true;
+            # home.packages {{{
+            home.packages = let
+              main = with pkgs; [
+                jq
+                gh
+                fd
+                ripgrep
 
-            userName = "Slava";
-            userEmail = "visla.vvi@gmail.com";
+                htop
 
-            delta = {
-              enable = true;
-              options = { syntax-theme = bat-theme; };
-            };
+                nixfmt
+                rnix-lsp
+                sumneko-lua-language-server
+                terraform-ls
 
-            extraConfig = {
-              pull = { rebase = true; };
-              core = { editor = "nvim"; };
-            };
+                neovim-package
+                xclip
 
-            ignores = [ ".DS_Store" "*.sw?" ];
-          };
-          # }}} git
-
-          # ssh {{{
-          programs.ssh = {
-            enable = true;
-            extraConfig = ''
-            Host *
-              AddKeysToAgent yes
-              IdentityFile ~/.ssh/id_ed25519
-
-            Host student
-              Hostname student.examus.net
-              User ci
-              IdentityFile ~/.ssh/id_student
-            '';
-          };
-          # }}} ssh
-
-          # kitty {{{
-          home.file.".config/kitty/kitty.conf" = {
-            source = ./kitty/kitty.conf;
-          };
-
-          # fish -lc is to setup env
-          home.file.".config/kitty/startup_session".text = ''
-            new_tab student
-            cd ~/10-19-Computer/11-Examus/11.01-student-web/
-            launch fish -lc 'nix-shell --run nvim'
-
-            new_tab compose
-            launch fish -lc 'nvim ~/10-19-Computer/13-Scratchpad/13.01-Text/(date +%Y-%m-%-d)-drafts'
-
-            new_tab dotfiles
-            cd ~/10-19-Computer/12-Tools/12.01-dotfiles/
-            launch fish -lc nvim
-          '';
-
-          home.file.".config/kitty/empty_session".text = ''
-            new_tab tab
-            cd
-            launch fish -lc nvim
-          '';
-          # }}} kitty
-
-          # stumpwm {{{
-          home.file.".config/stumpwm" = {
-            source = ./stumpwm;
-            recursive = true;
-          };
-          # }}} stumpwm
-
-          # starship {{{
-          programs.starship = {
-            enable = false;
-            enableFishIntegration = false;
-            settings = {
-              add_newline = false;
-              scan_timeout = 10;
-              nix_shell = { format = "[\\[nix\\]](blue) "; };
-
-              git_branch = { format = "[$symbol$branch](bold purple)"; };
-              git_status = {
-                style = "bold red";
-                format = lib.concatStrings [
-                  "["
-                  "$conflicted"
-                  "$deleted"
-                  "$renamed"
-                  "$modified"
-                  "$staged"
-                  "$untracked"
-
-                  "$ahead$ahead_count"
-                  "$behind$behind_count"
-                  "]"
-                  "($style)"
-                ];
-                ahead = "↑";
-                behind = "↓";
-                diverged = "⇅";
-                stashed = "";
-              };
-
-              directory = {
-                style = "bold green";
-                fish_style_pwd_dir_length = 1;
-                repo_root_style = "$style";
-              };
-              character = {
-                success_symbol = "[ ➤](green)";
-                error_symbol = "[ ➤](red)";
-              };
-              format = lib.concatStrings [
-                "$nix_shell"
-                "$username"
-                "$hostname"
-                "$directory"
-                "$git_branch"
-                "$git_state"
-                "$git_status"
-                "$cmd_duration"
-                "$character"
+                fira-code # for kitty
               ];
-            };
-          };
-          # }}} starship
+              node = with pkgs.nodePackages; [
+                vim-language-server
+                vscode-langservers-extracted
+                yaml-language-server
+              ];
+            in main ++ node;
+            # }}} home.packages
 
-          # fish {{{
-          home.file.".config/fish/functions" = {
-            source = ./fish/functions;
-            recursive = true;
-          };
-
-          programs.fish =
-            let
-              shellInit = ""
-                + "set -U VISUAL ${neovim-package}/bin/nvim"
-                + ''
-                  set -Ux NIX_PROFILES /nix/var/nix/profiles/default $HOME/.nix-profile
-                  fish_add_path /nix/var/nix/profiles/default/bin
-                  fish_add_path ~/.nix-profile/bin
-                  '';
-            in {
+            # {{{ fzf
+            programs.fzf = {
               enable = true;
-
-              plugins = [{
-                name = "forgit";
-                src = inputs.forgit;
-              }];
-
-              interactiveShellInit = shellInit;
-              loginShellInit = shellInit;
-
-              # shellAbbrs {{{
-              shellAbbrs = {
-                dc = "docker-compose";
-
-                gb = "git switch (git branch | string trim | fzf)";
-                gB = ''
-                   git switch (
-                    git fetch --all 1>/dev/null
-                    and git branch --all \
-                      | string replace 'remotes/origin/' "" \
-                      | string trim | sort | uniq \
-                      | fzf
-                  )'';
-
-                gBFG =
-                  "git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D";
-
-                ga = forgit::add;
-
-                gamend = "git commit --amend --no-edit";
-                gcom = "git commit";
-
-                gfa = "git fetch --all --prune --tags";
-                gpf = "git push --force-with-lease";
-                gpu = "git push -u origin HEAD";
-
-                gtop = "git rev-parse --show-toplevel";
-                gcd  = "cd (git rev-parse --show-toplevel)";
-                ghash = "git rev-parse --short HEAD";
-                gclean = "git clean -fd";
-
-                ginit = "git init ;and git commit -m 'root' --allow-empty";
-
-                grba = "git rebase --abort";
-                grbc = "git rebase --continue";
-                grbs = "git rebase --skip";
-
-                gsm = "git switch master";
-                gst = "git status --short --branch";
-
-                #
-                suspend = "systemctl suspend";
-                v = "nvim '+Term fish'";
-                weather = "curl wttr.in/guangzhou";
-              };
-              # }}} shellAbbrs
-
-          }; # }}} fish
-
-          # redshift {{{
-          services.redshift = {
-            enable = true; # TODO linux->true macos->false
-            latitude = 56.83;
-            longitude = 60.6;
-            temperature = {
-              day = 6500;
-              night = 3000;
+              enableFishIntegration = true;
             };
-          };
-          # }}} redshift
-        };
+            # }}} fzf
+
+            # {{{ bat
+            programs.bat = {
+              enable = true;
+              config = {
+                map-syntax = [ "*.json5:JavaScript (Babel)" ];
+                style = "numbers";
+                theme = bat-theme;
+              };
+            };
+            # }}} bat
+
+            # git {{{
+            programs.git = {
+              enable = true;
+              lfs.enable = true;
+
+              userName = "Slava";
+              userEmail = "visla.vvi@gmail.com";
+
+              delta = {
+                enable = true;
+                options = { syntax-theme = bat-theme; };
+              };
+
+              extraConfig = {
+                pull = { rebase = true; };
+                core = { editor = "nvim"; };
+              };
+
+              ignores = [ ".DS_Store" "*.sw?" ];
+            };
+            # }}} git
+
+            # ssh {{{
+            programs.ssh = {
+              enable = true;
+              extraConfig = ''
+              Host *
+                AddKeysToAgent yes
+                IdentityFile ~/.ssh/id_ed25519
+
+              Host student
+                Hostname student.examus.net
+                User ci
+                IdentityFile ~/.ssh/id_student
+              '';
+            };
+            # }}} ssh
+
+            # kitty {{{
+            home.file.".config/kitty/kitty.conf" = {
+              source = ./kitty/kitty.conf;
+            };
+
+            # fish -lc is to setup env
+            home.file.".config/kitty/startup_session".text = ''
+              new_tab student
+              cd ~/10-19-Computer/11-Examus/11.01-student-web/
+              launch fish -lc 'nix-shell --run nvim'
+
+              new_tab compose
+              launch fish -lc 'nvim ~/10-19-Computer/13-Scratchpad/13.01-Text/(date +%Y-%m-%-d)-drafts'
+
+              new_tab dotfiles
+              cd ~/10-19-Computer/12-Tools/12.01-dotfiles/
+              launch fish -lc nvim
+            '';
+
+            home.file.".config/kitty/empty_session".text = ''
+              new_tab tab
+              cd
+              launch fish -lc nvim
+            '';
+            # }}} kitty
+
+            # stumpwm {{{
+            home.file.".config/stumpwm" = {
+              source = ./stumpwm;
+              recursive = true;
+            };
+            # }}} stumpwm
+
+            # starship {{{
+            programs.starship = {
+              enable = false;
+              enableFishIntegration = false;
+              settings = {
+                add_newline = false;
+                scan_timeout = 10;
+                nix_shell = { format = "[\\[nix\\]](blue) "; };
+
+                git_branch = { format = "[$symbol$branch](bold purple)"; };
+                git_status = {
+                  style = "bold red";
+                  format = lib.concatStrings [
+                    "["
+                    "$conflicted"
+                    "$deleted"
+                    "$renamed"
+                    "$modified"
+                    "$staged"
+                    "$untracked"
+
+                    "$ahead$ahead_count"
+                    "$behind$behind_count"
+                    "]"
+                    "($style)"
+                  ];
+                  ahead = "↑";
+                  behind = "↓";
+                  diverged = "⇅";
+                  stashed = "";
+                };
+
+                directory = {
+                  style = "bold green";
+                  fish_style_pwd_dir_length = 1;
+                  repo_root_style = "$style";
+                };
+                character = {
+                  success_symbol = "[ ➤](green)";
+                  error_symbol = "[ ➤](red)";
+                };
+                format = lib.concatStrings [
+                  "$nix_shell"
+                  "$username"
+                  "$hostname"
+                  "$directory"
+                  "$git_branch"
+                  "$git_state"
+                  "$git_status"
+                  "$cmd_duration"
+                  "$character"
+                ];
+              };
+            };
+            # }}} starship
+
+            # fish {{{
+            home.file.".config/fish/functions" = {
+              source = ./fish/functions;
+              recursive = true;
+            };
+
+            programs.fish =
+              let
+                shellInit = ""
+                  + "set -U VISUAL ${neovim-package}/bin/nvim"
+                  + ''
+                    set -Ux NIX_PROFILES /nix/var/nix/profiles/default $HOME/.nix-profile
+                    fish_add_path /nix/var/nix/profiles/default/bin
+                    fish_add_path ~/.nix-profile/bin
+                    '';
+              in {
+                enable = true;
+
+                plugins = [{
+                  name = "forgit";
+                  src = inputs.forgit;
+                }];
+
+                interactiveShellInit = shellInit;
+                loginShellInit = shellInit;
+
+                # shellAbbrs {{{
+                shellAbbrs = {
+                  dc = "docker-compose";
+
+                  gb = "git switch (git branch | string trim | fzf)";
+                  gB = ''
+                     git switch (
+                      git fetch --all 1>/dev/null
+                      and git branch --all \
+                        | string replace 'remotes/origin/' "" \
+                        | string trim | sort | uniq \
+                        | fzf
+                    )'';
+
+                  gBFG =
+                    "git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D";
+
+                  ga = forgit::add;
+
+                  gamend = "git commit --amend --no-edit";
+                  gcom = "git commit";
+
+                  gfa = "git fetch --all --prune --tags";
+                  gpf = "git push --force-with-lease";
+                  gpu = "git push -u origin HEAD";
+
+                  gtop = "git rev-parse --show-toplevel";
+                  gcd  = "cd (git rev-parse --show-toplevel)";
+                  ghash = "git rev-parse --short HEAD";
+                  gclean = "git clean -fd";
+
+                  ginit = "git init ;and git commit -m 'root' --allow-empty";
+
+                  grba = "git rebase --abort";
+                  grbc = "git rebase --continue";
+                  grbs = "git rebase --skip";
+
+                  gsm = "git switch master";
+                  gst = "git status --short --branch";
+
+                  #
+                  suspend = "systemctl suspend";
+                  v = "nvim '+Term fish'";
+                  weather = "curl wttr.in/guangzhou";
+                };
+                # }}} shellAbbrs
+
+            }; # }}} fish
+
+            # redshift {{{
+            services.redshift = {
+              enable = true; # TODO linux->true macos->false
+              latitude = 56.83;
+              longitude = 60.6;
+              temperature = {
+                day = 6500;
+                night = 3000;
+              };
+            };
+            # }}} redshift
+          }
+        ];
+
       };
     };
   };
