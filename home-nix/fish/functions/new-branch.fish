bdb133fb45f45eb513ae86a1f05c4c8307892fef
Slava <visla.vvi@gmail.com>
2022-10-04 14:29:18 +0300

    fish: new-branch: master or main.

--- home-nix/fish/functions/new-branch.fish
+++ home-nix/fish/functions/new-branch.fish
@@ -1,5 +1,6 @@
 function new-branch --description 'Set up new branch: 1.create 2.push 3.MergeRequest' --argument issue topic
-  if test -z $issue -o -z $topic
+  if test -z "$issue" -o -z "$topic"
+    # Rebase on a fresh main branch.
     echo >&2 'Usage:   new-branch issue topic'
     echo >&2 'Example: new-branch acme-112 make-a-good-thing'
     return
@@ -10,6 +11,10 @@ function new-branch --description 'Set up new branch: 1.create 2.push 3.MergeReq
   set -l branch (string join '-' $issue $topic $date)
 
   git switch -c $branch
+
+  # Rebase on a fresh main branch.
+  git fetch origin master ;or git fetch origin main
+  git rebase origin/master ;or git rebase origin/main
 
   git push -o ci.skip -u origin $branch 2>&1 |
     grep merge_requests/new |
