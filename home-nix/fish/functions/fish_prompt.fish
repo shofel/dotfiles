56d0beb3df4260bb4df1006814451edf8547ba1c
Slava <visla.vvi@gmail.com>
2022-10-19 13:05:49 +0300

    fish: show untracked git files.

--- home-nix/fish/functions/fish_prompt.fish
+++ home-nix/fish/functions/fish_prompt.fish
@@ -7,8 +7,8 @@ function fish_prompt --description 'Write out the prompt'
 	# TODO check if the vars are already declared
 	# @see https://github.com/fish-shell/fish-shell/blob/master/share/tools/web_config/sample_prompts/classic_vcs.fish#L37
 
-	set -g __fish_git_prompt_show_informative_status 1
-	set -g __fish_git_prompt_hide_untrackedfiles 1
+	set -g __fish_git_prompt_show_informative_status true
+	set -g __fish_git_prompt_showuntrackedfiles true
 	set -g __fish_git_prompt_color_branch magenta --bold
 
 	set -g __fish_git_prompt_showupstream "informative"
@@ -16,7 +16,7 @@ function fish_prompt --description 'Write out the prompt'
 	set -g __fish_git_prompt_color_dirtystate blue
 	set -g __fish_git_prompt_color_stagedstate yellow
 	set -g __fish_git_prompt_color_invalidstate red
-	set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+	set -g __fish_git_prompt_color_untrackedfiles yellow
 	set -g __fish_git_prompt_color_cleanstate green --bold
 
 	set -g __fish_git_prompt_char_stateseparator ' '
