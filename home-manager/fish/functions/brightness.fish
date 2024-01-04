5a0aa4eb991c73eab342e41035d3e12895baee01
Slava <visla.vvi@gmail.com>
2024-01-04 18:02:43 +0300

    remove obsolete fish functions.

--- home-manager/fish/functions/brightness.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 2
-function brightness
-	echo $argv | sudo tee /sys/class/backlight/*/brightness
-end
--- home-manager/fish/functions/examus-refresh-session.fish
+++
@@ -1,4 +0,0 @@
-#
-function examus-refresh-session
-  ~/workspaces/Examus/simple-integration-client/simple_integration_client.py make
-end
