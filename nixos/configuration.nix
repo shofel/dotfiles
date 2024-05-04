13ce0067c2ff75e1dbbaba3077fd7284d119fe08
Slava <visla.vvi@gmail.com>
2024-05-04 08:32:02 +0500

    nixos: change locale language to C

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -130,18 +130,18 @@
   time.timeZone = "Europe/Istanbul";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "C.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "tr_TR.UTF-8";
-    LC_IDENTIFICATION = "tr_TR.UTF-8";
-    LC_MEASUREMENT = "tr_TR.UTF-8";
-    LC_MONETARY = "tr_TR.UTF-8";
-    LC_NAME = "tr_TR.UTF-8";
-    LC_NUMERIC = "tr_TR.UTF-8";
-    LC_PAPER = "tr_TR.UTF-8";
-    LC_TELEPHONE = "tr_TR.UTF-8";
-    LC_TIME = "tr_TR.UTF-8";
+    LC_ADDRESS = "C.UTF-8";
+    LC_IDENTIFICATION = "C.UTF-8";
+    LC_MEASUREMENT = "C.UTF-8";
+    LC_MONETARY = "C.UTF-8";
+    LC_NAME = "C.UTF-8";
+    LC_NUMERIC = "C.UTF-8";
+    LC_PAPER = "C.UTF-8";
+    LC_TELEPHONE = "C.UTF-8";
+    LC_TIME = "C.UTF-8";
   };
 
   # Enable the X11 windowing system.
