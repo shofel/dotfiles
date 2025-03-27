de92d14559f1dd1f046bf8b02e5f66dea528693b
Slava <visla.vvi@gmail.com>
2025-03-27 16:30:12 +0500

    nvim: remove the `devPlugins` option

--- neovim/mkNeovim.nix
+++ neovim/mkNeovim.nix
@@ -26,9 +26,6 @@ with lib;
     # Regexes for config files to ignore, relative to the nvim directory.
     # e.g. [ "^plugin/neogit.lua" "^ftplugin/.*.lua" ]
     ignoreConfigRegexes ? [],
-    # List of dev plugins (will be bootstrapped) - useful for plugin developers
-    # { name = <plugin-name>; url = <git-url>; }
-    devPlugins ? [],
     # TODO replace with wrapper:`autowrapRuntimeDeps` and `plugin.runtimeDeps`
     extraPackages ? [], # Extra runtime dependencies (e.g. ripgrep, ...)
 
@@ -133,7 +130,6 @@ with lib;
     # The final init.lua content that we pass to the Neovim wrapper.
     # It wraps the user init.lua, prepends the lua lib directory to the RTP
     # and prepends the nvim and after directory to the RTP
-    # It also adds logic for bootstrapping dev plugins (for plugin developers)
     initLua =
       ''
         vim.loader.enable()
@@ -142,26 +138,6 @@ with lib;
       ''
       # Wrap init.lua
       + (builtins.readFile ./nvim/init.lua)
-      # Bootstrap/load dev plugins
-      + optionalString (devPlugins != []) (
-        ''
-          local dev_pack_path = vim.fn.stdpath('data') .. '/site/pack/dev'
-          local dev_plugins_dir = dev_pack_path .. '/opt'
-          local dev_plugin_path
-        ''
-        + strings.concatMapStringsSep
-        "\n"
-        # TODO delete and replace with a recipe
-        (plugin: ''
-          dev_plugin_path = dev_plugins_dir .. '/${plugin.name}'
-          if vim.fn.empty(vim.fn.glob(dev_plugin_path)) > 0 then
-            vim.notify('Bootstrapping dev plugin ${plugin.name} ...', vim.log.levels.INFO)
-            vim.cmd('!${git}/bin/git clone ${plugin.url} ' .. dev_plugin_path)
-          end
-          vim.cmd('packadd! ${plugin.name}')
-        '')
-        devPlugins
-      )
       # Prepend nvim and after directories to the runtimepath
       # NOTE: This is done after init.lua,
       # because of a bug in Neovim that can cause filetype plugins
