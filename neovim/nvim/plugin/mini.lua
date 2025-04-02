8d41e7b21d8160ce89db1c23149b865f98b66e9f
Slava <slava.istomin@tuta.io>
2025-04-02 12:49:55 +0500

    nvim: restore `s`

--- neovim/nvim/plugin/mini.lua
+++ neovim/nvim/plugin/mini.lua
@@ -3,6 +3,4 @@ require('mini.comment').setup()
 require('mini.pairs').setup()
 
 require('mini.surround').setup()
-vim.keymap.set('n', 's', '<nop>') -- `s` is spoiled anyways
-
 require('mini.sessions').setup()
