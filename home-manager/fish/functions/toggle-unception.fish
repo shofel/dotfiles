3ab091419b2eec3a5f4b8a109e545a228f96572b
Slava <slava.istomin@tuta.io>
2025-04-03 18:11:55 +0500

    fish function to toggle unception

---
+++ home-manager/fish/functions/toggle-unception.fish
@@ -0,0 +1,23 @@
+function toggle-unception
+
+  # give them nice short names
+  set --function saved $NVIM_UNCEPTION_PIPE_PATH_HOST__SAVED
+  set --function active $NVIM_UNCEPTION_PIPE_PATH_HOST
+
+  # exactly one of them must be empty
+  if test -z "$saved"; and test -z "$active"
+    echo ERROR: both saved and active unception pipe paths are empty
+    return 2
+  end
+
+  # swap and rename back
+  set -g NVIM_UNCEPTION_PIPE_PATH_HOST__SAVED $active
+  set -g NVIM_UNCEPTION_PIPE_PATH_HOST $saved
+
+  if test -z "$NVIM_UNCEPTION_PIPE_PATH_HOST"
+    set -ge NVIM_UNCEPTION_PIPE_PATH_HOST
+    echo unception disabled
+  else
+    echo unception enabled
+  end
+end
