be10311ac56ea863c1cb2fccd121c9164f39b87a
Slava <slava.istomin@tuta.io>
2025-04-09 15:40:44 +0500

    nvim: setup mini.trailspace

--- neovim/nvim/plugin/mini.lua
+++ neovim/nvim/plugin/mini.lua
@@ -4,3 +4,6 @@ require('mini.pairs').setup()
 
 require('mini.surround').setup()
 require('mini.sessions').setup()
+require('mini.trailspace').setup()
+
+vim.keymap.set('n', '<space>tw', MiniTrailspace.trim, {unique = true})
