f74b69728beb9e1cf9b8b82f292bae5d11b1ae75
Slava <slava.istomin@tuta.io>
2025-11-18 11:34:28 +0500

    nixos: extract a dir with hardware conf

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -20,9 +20,6 @@
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
 
-    # Import your generated (nixos-generate-config) hardware configuration
-    ./hardware-configuration.nix
-
     # Import home-manager's NixOS module
     inputs.home-manager.nixosModules.home-manager
 
