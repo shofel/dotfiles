3418d35e6e6139712060da08f4fd32c979af9812
Slava <slava.istomin@tuta.io>
2025-12-16 05:59:04 +0000

    add slava to `dialout` group

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -81,7 +81,7 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      extraGroups = ["networkmanager" "wheel" "docker"];
+      extraGroups = ["networkmanager" "wheel" "docker" "dialout"];
     };
     nixosvmtest = {
       isSystemUser = true;
